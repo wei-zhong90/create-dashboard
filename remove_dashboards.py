86a3ff3cc6a4fb2f745064288cb7d89bc2118fe4
Zhong <weiaws@amazon.com>
2023-01-10 10:44:58 +0800

    added the paginator for remove function

--- remove_dashboards.py
+++ remove_dashboards.py
@@ -2,9 +2,21 @@ import boto3
 
 client = boto3.client('cloudwatch')
 
-response = client.list_dashboards()
+paginator = client.get_paginator('list_dashboards')
 
-dashboard_list = [dashboard['DashboardName'] for dashboard in response['DashboardEntries']]
+# response = client.list_dashboards()
+
+response_iterator = paginator.paginate(
+    PaginationConfig={
+        'MaxItems': 200
+    }
+)
+
+dashboard_list = []
+
+for page in response_iterator:
+    dashboard_list.extend(da['DashboardName'] for da in page['DashboardEntries'])
+# dashboard_list = [dashboard['DashboardName'] for dashboard in response['DashboardEntries']]
 
 client.delete_dashboards(
     DashboardNames = dashboard_list
